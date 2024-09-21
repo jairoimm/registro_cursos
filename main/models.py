08f9ba7735acb457496e8de71464a71d25b82e22
jairo munoz <jairo.imm@gmail.com>
2024-09-21 19:56:09 -0300

    se crean los modelos

--- main/models.py
+++ main/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
 # Create your models here.
+
+class Estudiante(models.Model):
+    rut = models.CharField(max_length=9, primary_key=True)
+    nombre = models.CharField(max_length=50, null=False)
+    apellido = models.CharField(max_length=50, null=False)
+    fecha_nac = models.DateField()
+    activo = models.BooleanField(default=False)
+    creacion_registro = models.DateTimeField(auto_now_add=True)
+    modificacion_registro = models.DateTimeField(auto_now=True)
+    creado_por = models.CharField(max_length=50, null=False)
+
+class Direccion(models.Model):
+    calle = models.CharField(max_length=50, null=False)
+    numero = models.CharField(max_length=10, null=False)
+    dpto = models.CharField(max_length=10, null=True)
+    comuna = models.CharField(max_length=50, null=False)
+    regiones = models.CharField(max_length=50, null=False)
+    estudiante = models.OneToOneField(
+        Estudiante, 
+        related_name='direccion', 
+        on_delete=models.CASCADE
+        )
+
+class Profesor(models.Model):
+    rut = models.CharField(max_length=9, primary_key=True)
+    nombre = models.CharField(max_length=50, null=False)
+    apellido = models.CharField(max_length=50, null=False)
+    activo = models.BooleanField(default=False)
+    creacion_registro = models.DateTimeField(auto_now_add=True)
+    modificacion_registro = models.DateTimeField(auto_now=True)
+    creado_por = models.CharField(max_length=50, null=False)
+    
+class Curso(models.Model):
+    codigo = models.CharField(max_length=10, primary_key=True)
+    nombre = models.CharField(max_length=50, null=False)
+    version = models.IntegerField()
+    profesor = models.ManyToManyField(
+        Profesor, 
+        related_name='cursos',
+        )
+    estudiantes = models.ForeignKey(
+        Estudiante, 
+        related_name='cursos',
+        on_delete=models.RESTRICT
+        )
